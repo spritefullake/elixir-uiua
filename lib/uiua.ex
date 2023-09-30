70cef448674677dd420e411f2b1b3008670ba742
Talha Ahmad <gamezen@icloud.com>
2023-09-30 11:27:45 -0400

    added single argument restack and restack operator

--- lib/uiua.ex
+++ lib/uiua.ex
@@ -31,15 +31,14 @@ defmodule Uiua do
   defmacro make_arrayable(func, first, second) do
     quote do
       cond do
-        is_list(unquote(first)) and is_list(unquote(second))
-        and unquote(Enum.count(first) == Enum.count(second))
-          ->
-            quote do
-
+        is_list(unquote(first)) and is_list(unquote(second)) and
+            unquote(Enum.count(first) == Enum.count(second)) ->
+          quote do
             pairwise_operate(unquote(func), unquote(first), unquote(second))
-            end
-        true
-          -> nil
+          end
+
+        true ->
+          nil
       end
     end
   end
@@ -48,14 +47,15 @@ defmodule Uiua do
     cond do
       is_list(y) and is_list(x) and Enum.count(x) == Enum.count(y) ->
         pairwise_operate(&__MODULE__.add/1, x, y)
+
       is_list(y) ->
         result = Enum.map(y, fn elem -> elem + x end)
         [result | rest]
+
       true ->
         [y + x | rest]
     end
   end
-
 
   def subtract([x, y | rest]) do
     [y - x | rest]
@@ -66,32 +66,39 @@ defmodule Uiua do
   end
 
   def restack(indices, stack) do
-    top = for index <- indices do
-      Enum.at(stack, index)
-    end
-
-    new_stack = Enum.with_index(stack, fn x, stack_index ->
-      if Enum.member?(indices, stack_index) do
-        nil
-      else
-        x
+    top =
+      for index <- indices do
+        Enum.at(stack, index)
       end
-    end) |> Enum.filter(fn x -> x != nil end)
-    |> Enum.drop(Enum.min(indices))
+
+    new_stack =
+      Enum.with_index(stack, fn x, stack_index ->
+        if Enum.member?(indices, stack_index) do
+          nil
+        else
+          x
+        end
+      end)
+      |> Enum.filter(fn x -> x != nil end)
+      |> Enum.drop(Enum.min(indices))
 
     top ++ new_stack
   end
 
+  def restack([indices | stack]) do
+    restack(indices, stack)
+  end
+
   def duplicate(stack) do
-    restack([0,0], stack)
+    restack([0, 0], stack)
   end
 
   def over(stack) do
-    restack([1,0,1], stack)
+    restack([1, 0, 1], stack)
   end
 
   def flip(stack) do
-    restack([1,0], stack)
+    restack([1, 0], stack)
   end
 
   def pop(stack) do
@@ -99,21 +106,20 @@ defmodule Uiua do
   end
 
   def roll(stack) do
-    restack([1,2,0], stack)
+    restack([1, 2, 0], stack)
   end
 
   def unroll(stack) do
-    restack([2,0,1], stack)
+    restack([2, 0, 1], stack)
   end
 
   def noop(stack) do
     [nil | stack]
   end
 
-  def not(stack) do
+  def not stack do
     [1 | stack] |> flip |> subtract
   end
-
 
   @keymap %{
     "+" => &__MODULE__.add/1,
@@ -122,7 +128,8 @@ defmodule Uiua do
     "⇌" => &Enum.reverse/1,
     "." => &__MODULE__.duplicate/1,
     "," => &__MODULE__.over/1,
-    "·" => &__MODULE__.noop/1
+    "·" => &__MODULE__.noop/1,
+    "⇵" => &__MODULE__.restack/1
   }
 
   def eval(terms) do
--- test/uiua_test.exs
+++ test/uiua_test.exs
@@ -7,20 +7,27 @@ defmodule UiuaTest do
   end
 
   test "restack" do
-    assert Uiua.restack([0, 0], [1,2,3]) == [1,1,2,3]
-    assert Uiua.restack([1,0,2,2],[1,2,3]) == [2, 1, 3, 3]
-    assert Uiua.restack([1,2,0], [1,2,3]) == [2,3,1]
-    assert Uiua.restack([1], [1,2,3]) == [2,3]
+    assert Uiua.restack([0, 0], [1, 2, 3]) == [1, 1, 2, 3]
+    assert Uiua.restack([1, 0, 2, 2], [1, 2, 3]) == [2, 1, 3, 3]
+    assert Uiua.restack([1, 2, 0], [1, 2, 3]) == [2, 3, 1]
+    assert Uiua.restack([1], [1, 2, 3]) == [2, 3]
+  end
+
+  test "restack one argument is same as two argument restack" do
+    assert Uiua.restack([[0, 0], 1, 2, 3]) == Uiua.restack([0, 0], [1, 2, 3])
+    assert Uiua.restack([[1, 0, 2, 2], 1, 2, 3]) == Uiua.restack([1, 0, 2, 2], [1, 2, 3])
+    assert Uiua.restack([[1, 2, 0], 1, 2, 3]) == Uiua.restack([1, 2, 0], [1, 2, 3])
+    assert Uiua.restack([[1], 1, 2, 3]) == Uiua.restack([1], [1, 2, 3])
   end
 
   test "add" do
     assert Uiua.run("+ 1 2") == [3]
-    assert Uiua.run("+1 [2 3 4]") == [3,4,5]
+    assert Uiua.run("+1 [2 3 4]") == [3, 4, 5]
   end
 
   test "not" do
     assert Uiua.not([0]) == [1]
     assert Uiua.not([1]) == [0]
-    assert Uiua.not([[0, 1, 1, 0]]) == [[1,0,0,1]]
+    assert Uiua.not([[0, 1, 1, 0]]) == [[1, 0, 0, 1]]
   end
 end
