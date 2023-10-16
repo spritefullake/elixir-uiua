4f30b7647b4002f35834717b5597bd0eb7387fa4
Talha Ahmad <42ahmad@gmail.com>
2023-10-16 00:14:26 -0400

    cleaned up stack.ex and added dialyzer

--- lib/stack.ex
+++ lib/stack.ex
@@ -1,7 +1,17 @@
 defmodule Stack do
+  @moduledoc """
+  The stack module defines the basic list of elements (aka an array, or a "stack")
+  that the Uiua language operates with.
+  """
 
+  @spec only_first_elem_not_list_rest_same_size([...]) :: boolean
+  def only_first_elem_not_list_rest_same_size([first | rest]) do
+    Kernel.not(is_list(first)) and same_size_lists(rest)
+  end
 
-  @spec pairwise_operate(function(), list(), list()) :: list
+  @doc"""
+  Pairs elements in lists and applies the function to them"""
+  @spec pairwise_operate((a, b -> any()), [a], [b]) :: list when a: var, b: var
   def pairwise_operate(func, first, second) do
     for i <- 0..(Enum.count(first) - 1) do
       apply(func, [Enum.at(first, i), Enum.at(second, i)])
@@ -9,24 +19,10 @@ defmodule Stack do
     |> List.flatten()
   end
 
-  @spec pervasive_operate(function(), any, list()) :: list
-  def pervasive_operate(func, first, second) do
-    for i <- second do
-      apply(func, [first, i])
-    end
-    |> List.flatten()
-  end
-
-  @spec same_size_lists(list(), list()) :: boolean
-  def same_size_lists(x, y) do
-    is_list(y) and is_list(x) and Enum.count(x) == Enum.count(y)
-  end
-
-  def only_first_elem_not_list_rest_same_size([first | rest]) do
-    Kernel.not(is_list(first)) and same_size_lists(rest)
-  end
-
-  @spec elementwise_operate(function(), [list]) :: list
+  @doc"""
+  Applies a single element across the whole list. For example ```+ 3 [1,2,3,4]```
+  """
+  @spec elementwise_operate((a -> any()), [a]) :: list when a: var
   def elementwise_operate(func, args) do
     for elems <- List.zip(args) do
       apply(func, elems |> Tuple.to_list())
@@ -35,37 +31,16 @@ defmodule Stack do
 
   @spec same_size_lists(list()) :: boolean
   def same_size_lists(args) do
-    # if any element of the list is NOT a list
     if args |> Enum.reject(&is_list/1) == [] do
-      if args |> Enum.reject(fn x -> is_list(x) end) == [] do
-        sizes = args |> Enum.map(&Enum.count/1)
-
-        if sizes |> Enum.uniq() |> Enum.count() == 1 do
-          true
-        else
-          false
-        end
-      else
-        false
-      end
-    else
+      # make sure lists are the same size
+      args |> Enum.map(&Enum.count/1) |> Enum.uniq() |> Enum.count() == 1
+    else # if not all the items are lists
       false
     end
   end
 
-  def arrayize(func, first, second) do
-    cond do
-      same_size_lists(first, second) ->
-        pairwise_operate(func, first, second)
 
-      is_list(second) ->
-        pervasive_operate(func, first, second)
-
-      true ->
-        func.(first, second)
-    end
-  end
-
+  @spec arrayize(function(), list()) :: list()
   def arrayize(func, args) do
     cond do
       same_size_lists(args) ->
